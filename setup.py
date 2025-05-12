97d6973f4a2b851c8302ef8ffde74797f9f13943
Izhar Ahmad <thisisizharahmad@gmail.com>
2025-05-13 01:06:31 +0500

    Add pyproject.toml for project metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
 from setuptools import setup
 
-import prept
-
-with open('requirements.txt', 'r') as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name='prept',
-    version=prept.__version__,
-    py_modules=['prept'],
-    install_requires=requirements,
-    entry_points={
-        'console_scripts': ['prept = prept.cli.main:cli'],
-    },
-)
+setup()
